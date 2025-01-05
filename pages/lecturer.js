bb44ed6e993ea413622294db533fa1e52b2c42ce
Amy Mcevoy <G00413220@atu.ie>
2025-01-05 18:47:50 +0000

    Testing mongodb

--- pages/lecturer.js
+++ pages/lecturer.js
@@ -6,5 +6,16 @@ const { MongoClient, ObjectId } = require('mongodb');
 const mongoUrl = 'mongodb://localhost:27017';
 const dbName = 'proj2024MongoDB'; 
 
+let mongoDB;
+// Connect to MongoDB
+MongoClient.connect(mongoUrl, (err, client) => {
+    if (err) {
+        console.error('Error connecting to MongoDB:', err);
+        return;
+    }
+    console.log('Connected to MongoDB');
+    mongoDB = client.db(dbName);
+});
+
 
 module.exports = router;
